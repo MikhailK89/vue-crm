3339d2cc25f4f366b7d46dcc4180fc8d1239b1d4
Mikhail <kravch_mikhail@mail.ru>
2020-08-13 02:02:49 +0300

    pages with routing

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="app-main-layout">
+
+    <Navbar @click="isOpen = !isOpen" />
+
+    <Sidebar v-model="isOpen" />
+
+    <main class="app-content" :class="{full: !isOpen}">
+      <div class="app-page">
+        <router-view />
+      </div>
+    </main>
+
+    <div class="fixed-action-btn">
+      <a class="btn-floating btn-large blue" href="#">
+        <i class="large material-icons">add</i>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/app/Navbar'
+import Sidebar from '@/components/app/Sidebar'
+
+export default {
+  name: 'main-layout',
+  data: () => ({
+    isOpen: true
+  }),
+  components: {
+    Navbar, Sidebar
+  }
+}
+</script>
