e093eaced2363d4ab8c36249cbea77bac3db68be
Mikhail <kravch_mikhail@mail.ru>
2020-08-14 20:45:55 +0300

    Register and login validation

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import dateFilter from '@/filters/date.filter'
+import messagePlugin from '@/utils/message.plugin'
+import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
 Vue.config.productionTip = false
+
+Vue.use(messagePlugin)
+Vue.use(Vuelidate)
+Vue.filter('date', dateFilter)
 
 new Vue({
   router,
---
+++ src/utils/message.plugin.js
@@ -0,0 +1,11 @@
+export default {
+  install(Vue, options) {
+    Vue.prototype.$message = function(html) {
+      window.M.toast({html})
+    }
+
+    Vue.prototype.$error = function(html) {
+      window.M.toast({html: `[Ошибка]: ${html}`})
+    }
+  }
+}
