e1386c383c29535bb488e47b17dd551546482596
Mikhail <kravch_mikhail@mail.ru>
2020-08-21 22:00:44 +0300

    Profile page and en-ru localization

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import store from './store'
 import dateFilter from '@/filters/date.filter'
 import currencyFilter from '@/filters/currency.filter'
+import localizeFilter from '@/filters/localize.filter'
 import tooltipDirective from '@/directives/tooltip.directive'
 import messagePlugin from '@/utils/message.plugin'
 import Loader from '@/components/app/Loader'
@@ -22,6 +23,7 @@ Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 Vue.filter('currency', currencyFilter)
+Vue.filter('localize', localizeFilter)
 Vue.directive('tooltip', tooltipDirective)
 Vue.component('Loader', Loader)
 Vue.component('Paginate', Paginate)
