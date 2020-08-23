ba40a12bdf3def43996737d50b06b66f9dfb5f16
Mikhail <kravch_mikhail@mail.ru>
2020-08-24 01:18:55 +0300

    Config files have been hidden

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ import tooltipDirective from '@/directives/tooltip.directive'
 import messagePlugin from '@/utils/message.plugin'
 import titlePlugin from '@/utils/title.plugin'
 import Loader from '@/components/app/Loader'
+import firebaseConfig from '@/config/firebase.config'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
@@ -31,17 +32,6 @@ Vue.filter('localize', localizeFilter)
 Vue.directive('tooltip', tooltipDirective)
 Vue.component('Loader', Loader)
 Vue.component('Paginate', Paginate)
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDxKGDj44PknepHXl1vEMzNXK8GgoL6GXs",
-  authDomain: "vue-crm-db.firebaseapp.com",
-  databaseURL: "https://vue-crm-db.firebaseio.com",
-  projectId: "vue-crm-db",
-  storageBucket: "vue-crm-db.appspot.com",
-  messagingSenderId: "859678497698",
-  appId: "1:859678497698:web:46c2de937f2375401530fb",
-  measurementId: "G-P30QZ5ZLKP"
-}
 
 firebase.initializeApp(firebaseConfig)
 
